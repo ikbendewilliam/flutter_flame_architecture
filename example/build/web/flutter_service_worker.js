9bc9614528c229b31f00fc06f8934a1786ae2216
William Verhaeghe <ikbendewilliam@hotmail.com>
2021-06-09 22:16:41 +0200

    test build with ./ as base

--- example/build/web/flutter_service_worker.js
+++ example/build/web/flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "04df03b38db30c0f2e7f6693e9021e51",
-"/": "04df03b38db30c0f2e7f6693e9021e51",
+"index.html": "d0446bb57eef5b39f29e9258fd74a4a6",
+"/": "d0446bb57eef5b39f29e9258fd74a4a6",
 "main.dart.js": "e2417a202f9f399a654def616a3462ff",
 "manifest.json": "9e7b34fd7c291ca2523123ef20497930",
 "version.json": "4b6db237b3514a88107a422469adfb0f"
